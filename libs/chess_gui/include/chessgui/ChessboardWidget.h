348a7cd40a56368c65f67fbfd3ef5722a038c16f
Florian Giesemann <fgiesemann@proton.me>
2025-09-20 11:44:45 +0200

    Setup project

---
+++ libs/chess_gui/include/chessgui/ChessboardWidget.h
@@ -0,0 +1,11 @@
+/* ************************************************************************** *
+ * Chess Gui                                                                  *
+ * Graphical user interface components for chess games                        *
+ * ************************************************************************** */
+
+#ifndef CHESSGUI_CHESSBOARDWIDGET_H
+#define CHESSGUI_CHESSBOARDWIDGET_H
+
+namespace chessgui {}
+
+#endif
---
+++ libs/chess_gui/src/ChessboardWidget.cpp
@@ -0,0 +1,8 @@
+/* ************************************************************************** *
+ * Chess Gui                                                                  *
+ * Graphical user interface components for chess games                        *
+ * ************************************************************************** */
+
+#include "chessgui/ChessboardWidget.h"
+
+namespace chessgui {}
