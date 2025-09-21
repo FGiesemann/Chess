c820527fc3e377defcc3304f5b620462e57d137f
Florian Giesemann <fgiesemann@proton.me>
2025-09-21 17:57:18 +0200

    Create MainWindow class and implement two-click move logic

--- libs/chess_gui/testapp/main.cpp
+++ libs/chess_gui/testapp/main.cpp
@@ -2,44 +2,108 @@
 #include "chessgui/PieceSet.h"
 
 #include <QApplication>
+#include <QKeyEvent>
 #include <QMainWindow>
+#include <QVBoxLayout>
+#include <QWidget>
 
 #include <iostream>
 
-chessgui::ChessboardWidget *chessboard;
-chesscore::Position<chesscore::Bitboard> position;
+namespace chessgui {
 
-auto squareClicked(const chesscore::Square &square) -> void {
-    chessboard->clearMarkedSquares();
-    const auto opt_piece = position.board().get_piece(square);
-    if (opt_piece.has_value()) {
-        const auto all_legal_moves = position.all_legal_moves();
-        for (const auto &move : all_legal_moves) {
-            if (move.from == square) {
-                chessboard->markSquare(move.to);
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(const PieceSet &pieceSet, QWidget *parent = nullptr)
+        : QMainWindow(parent), m_boardWidget{new ChessboardWidget(pieceSet, this)}, m_currentPosition{chesscore::FenString::starting_position()}, m_selectedSquare(std::nullopt) {
+        setWindowTitle("Schachbrett");
+        setMinimumSize(400, 400);
+        resize(600, 600);
+
+        QWidget *centralWidget = new QWidget(this);
+        auto *layout = new QVBoxLayout(centralWidget);
+        layout->addWidget(m_boardWidget);
+        setCentralWidget(centralWidget);
+
+        m_boardWidget->setPosition(m_currentPosition);
+
+        connect(m_boardWidget, &ChessboardWidget::squareClicked, this, &MainWindow::onSquareClicked);
+    }
+protected:
+    auto keyPressEvent(QKeyEvent *event) -> void override {
+        if (event->key() == Qt::Key_Escape) {
+            if (m_selectedSquare.has_value()) {
+                m_boardWidget->clearMarkedSquares();
+                m_selectedSquare = std::nullopt;
+                m_legal_moves.clear();
             }
         }
     }
-}
+private slots:
+    auto onSquareClicked(const chesscore::Square &square) -> void {
+        if (m_selectedSquare.has_value()) {
+            chesscore::Square from_square = m_selectedSquare.value();
+            qDebug() << "Wants to move from " << to_string(from_square) << " to " << to_string(square);
+
+            auto iter = std::find_if(m_legal_moves.begin(), m_legal_moves.end(), [&](const chesscore::Move &move) { return move.to == square; });
+            auto piece_at_target_square = m_currentPosition.board().get_piece(square);
+            if (iter != m_legal_moves.end()) {
+                m_currentPosition.make_move(*iter);
+                m_boardWidget->setPosition(m_currentPosition);
+                m_boardWidget->clearMarkedSquares();
+                m_selectedSquare = std::nullopt;
+                m_legal_moves.clear();
+            } else if (piece_at_target_square.has_value()) {
+                m_boardWidget->clearMarkedSquares();
+                m_legal_moves.clear();
+                m_selectedSquare = std::nullopt;
+                const auto opt_piece = m_currentPosition.board().get_piece(square);
+                if (opt_piece.has_value()) {
+                    m_selectedSquare = square;
+                    const auto all_legal_moves = m_currentPosition.all_legal_moves();
+                    for (const auto &move : all_legal_moves) {
+                        if (move.from == square) {
+                            m_boardWidget->markSquare(move.to);
+                            m_legal_moves.push_back(move);
+                        }
+                    }
+                }
+            } else {
+                m_boardWidget->clearMarkedSquares();
+                m_legal_moves.clear();
+                m_selectedSquare = std::nullopt;
+            }
+        } else {
+            m_boardWidget->clearMarkedSquares();
+            m_legal_moves.clear();
+            const auto opt_piece = m_currentPosition.board().get_piece(square);
+            if (opt_piece.has_value() && opt_piece.value().color == m_currentPosition.side_to_move()) {
+                m_selectedSquare = square;
+                const auto all_legal_moves = m_currentPosition.all_legal_moves();
+                for (const auto &move : all_legal_moves) {
+                    if (move.from == square) {
+                        m_boardWidget->markSquare(move.to);
+                        m_legal_moves.push_back(move);
+                    }
+                }
+            }
+        }
+    }
+private:
+    ChessboardWidget *m_boardWidget;
+    chesscore::Position<chesscore::Bitboard> m_currentPosition;
+    std::optional<chesscore::Square> m_selectedSquare;
+    chesscore::MoveList m_legal_moves;
+};
+
+} // namespace chessgui
 
 auto main(int argc, char *argv[]) -> int {
     QApplication a(argc, argv);
-    QMainWindow window;
-    window.setWindowTitle("Chessboard App");
-    window.setMinimumSize(400, 400);
-    window.resize(600, 600);
-
-    position = chesscore::Position<chesscore::Bitboard>{chesscore::FenString::starting_position()};
-
     chessgui::PieceSet pieces{QString{"D:/Programmierung/Projekte/Chess/ChessGui/data/pieces/alpha"}};
-
-    chessboard = new chessgui::ChessboardWidget(pieces, &window);
-    chessboard->setPosition(position);
-
-    QObject::connect(chessboard, &chessgui::ChessboardWidget::squareClicked, squareClicked);
-
-    window.setCentralWidget(chessboard);
+    chessgui::MainWindow window(pieces);
     window.show();
 
     return a.exec();
 }
+#include "main.moc"
