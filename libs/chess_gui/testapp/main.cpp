00cf17304633e83b353c48eb3252efebfe218541
Florian Giesemann <fgiesemann@proton.me>
2025-09-21 13:24:23 +0200

    Show possible target squares for selected piece

--- libs/chess_gui/testapp/main.cpp
+++ libs/chess_gui/testapp/main.cpp
@@ -7,9 +7,19 @@
 #include <iostream>
 
 chessgui::ChessboardWidget *chessboard;
+chesscore::Position<chesscore::Bitboard> position;
 
 auto squareClicked(const chesscore::Square &square) -> void {
-    chessboard->markSquare(square);
+    chessboard->clearMarkedSquares();
+    const auto opt_piece = position.board().get_piece(square);
+    if (opt_piece.has_value()) {
+        const auto all_legal_moves = position.all_legal_moves();
+        for (const auto &move : all_legal_moves) {
+            if (move.from == square) {
+                chessboard->markSquare(move.to);
+            }
+        }
+    }
 }
 
 auto main(int argc, char *argv[]) -> int {
@@ -19,10 +29,12 @@ auto main(int argc, char *argv[]) -> int {
     window.setMinimumSize(400, 400);
     window.resize(600, 600);
 
+    position = chesscore::Position<chesscore::Bitboard>{chesscore::FenString::starting_position()};
+
     chessgui::PieceSet pieces{QString{"D:/Programmierung/Projekte/Chess/ChessGui/data/pieces/alpha"}};
 
     chessboard = new chessgui::ChessboardWidget(pieces, &window);
-    chessboard->setPosition(chesscore::Position<chesscore::Bitboard>{chesscore::FenString::starting_position()});
+    chessboard->setPosition(position);
 
     QObject::connect(chessboard, &chessgui::ChessboardWidget::squareClicked, squareClicked);
 
